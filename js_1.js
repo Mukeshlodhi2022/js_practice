1b269f16baba6b5aae902ed20c25345d182c3c52
Mukesh Lodhi <lodhimukesh63@gmail.com>
2024-07-19 15:51:03 +0530

    Add files via upload

---
+++ js_1.js
@@ -0,0 +1,9 @@
+// const we can't change the values ones it is decleared 
+// let we can change the values into the block scope (most used)
+// var we can change the values into both (global level)
+
+console.log("hi");
+const pi = 3.14;
+let name = "mukesh ";
+var hi = "hello";
+hi = "namaste";
