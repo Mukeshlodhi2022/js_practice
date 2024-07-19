623d59a83b9629ca051088e3bd26e1015f883c27
Mukesh Lodhi <lodhimukesh63@gmail.com>
2024-07-19 15:59:42 +0530

    Add files via upload

---
+++ js_2.js
@@ -0,0 +1,8 @@
+// datatypes
+// num = 1,1,2;
+// string = "mukesh"
+// boolean = true/false;
+// bigint = store more values 
+// null = empty value;
+// undefined = nothing 
+// typeof (for cheaking the type of variable );
