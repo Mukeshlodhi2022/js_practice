dece7252e7c0980671555e98383fb81e1258eba7
Mukesh Lodhi <lodhimukesh63@gmail.com>
2024-07-20 22:35:41 +0530

    Add files via upload

---
+++ js_3.js
@@ -0,0 +1,10 @@
+// type conversion 
+let a = "5";
+let b = Number(a);
+console.log(typeof b);
+// "5" = Number(5);
+// 5  = String(5);
+// 1 = Boolean(1);
+// "33a" = Number(33a); ==> give NaN not a number and it is a number in type;
+// console.log("3" + 5 + 5); ==> "355";
+// console.log(5+5+"3" ) == > "105"
